e30489a16b88a5531585d2f588187cb785778467
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-07-21 14:23:34 +0000

    Fri, Jul 21, 2023, 6:23 PM +04:00

--- mario.c
+++ mario.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#include <cs50.h>
 
 
-// This is program which emulates mario.
 
-int main(void)
-{
-    const int size = 10;
+int main(void){
+    //Size of brick wall
+    const int size = get_int("Size: ");
+
     for (int i=0; i<size; i++)
      {
         for (int j=0; j<size; j++){
         printf("o");
-
-        }
+       }
         printf("\n");
     }
 }
 
+void print_grid(int n) {
+    
+}
