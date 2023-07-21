dcea04ada5c99a19d9511d473b77c30afd997d25
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-07-21 11:27:49 +0000

    Fri, Jul 21, 2023, 11:27 AM +00:00

--- hello.c
+++ hello.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
-    string answer = get_string("What is your name? ");
-    printf("hello, %s\n", answer);
+    string answer1 = get_string("What is your name? ");
+    string answer2 = get_string("What is your gf's name? ");
+    printf("hello, %s and %s\n", answer1, answer2);
 }
