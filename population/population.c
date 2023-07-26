dc3698fea7796c66231764e6011d574a39fba724
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-07-26 16:49:54 +0000

    Wed, Jul 26, 2023, 8:49 PM +04:00

--- population/population.c
+++ population/population.c
@@ -6,8 +6,6 @@ int main(void)
 
     // TODO: Prompt for start size
     int n;
-    int m;
-    int upscale;
     do
     {
         n = get_int ("Starting Size: ");
@@ -25,7 +23,7 @@ int main(void)
 
     // TODO: Calculate number of years until we reach threshold
 
-    int upscale = n + (n/3) + (m/4)
+    int upscale = n + (n/3) + (m/4);
 
     // TODO: Print number of years
 
