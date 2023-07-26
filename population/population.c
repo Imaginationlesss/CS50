8f829bcab793070f1b0d589124ff8f23041b732e
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-07-26 18:40:18 +0000

    Wed, Jul 26, 2023, 10:40 PM +04:00

--- population/population.c
+++ population/population.c
@@ -25,15 +25,23 @@ int main(void)
     // TODO: Calculate number of years until we reach threshold
 
     int years = 0;
-    
+
+    if (n == m)
+    {
+
+        printf("Years: %i\n", years);
+    }
+    else
+    {
+
     do
     {
         n = n + (n / 3) - (n / 4);
         years++;
     }
     while (n < m);
-
     }
     // TODO: Print number of years
 
     printf("Years: %i\n", years);
+}
