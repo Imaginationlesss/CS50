007fc84b60c74769a6d18d6d4c8bd05ac1262c6e
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-07-28 22:47:43 +0000

    Sat, Jul 29, 2023, 2:47 AM +04:00

--- half/half.c
+++ half/half.c
@@ -18,6 +18,7 @@ int main(void)
 // TODO: Complete the function
 float half(float bill, float tax, int tip)
 {
-    Finale = bill + bill * tax + bill * tip;
-    return 0.0;
+    int finale = 0;
+    finale = bill * tax + bill * tip;
+    return finale;
 }
