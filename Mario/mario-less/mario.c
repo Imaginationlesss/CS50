3998115f40e003f41b47767d87cca87cd4430afb
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-07-29 00:01:37 +0000

    Sat, Jul 29, 2023, 4:01 AM +04:00

--- Mario/mario-less/mario.c
+++ Mario/mario-less/mario.c
@@ -3,6 +3,13 @@
 
 int main(void)
 {
-    int height = get_int("Height: ");
-    printf = ()
+    int height = 0;
+
+    do
+    {
+        
+    height = get_int("Height: ");
+
+    }
+    while (height > 0 || height < 9);
 }
