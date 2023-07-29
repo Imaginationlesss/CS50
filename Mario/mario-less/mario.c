43b0012e5fd9b5613f1952fb49615e8e728a0372
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-07-29 01:11:07 +0000

    Sat, Jul 29, 2023, 5:11 AM +04:00

--- Mario/mario-less/mario.c
+++ Mario/mario-less/mario.c
@@ -20,11 +20,12 @@ int main(void)
 
     for (int i = 0; i < height; i++)
      {
-        for(int j = 0; j < height; j++){
+        for(int j = 0; j < block; j++){
 
         printf("#");
-        ++;
+        block--;
 
     }
     printf("\n");
+}
 }
