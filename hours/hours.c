01277a0c01948d9c6e4b546ea589aa8b8dc1bc32
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-08-02 13:55:40 +0000

    Wed, Aug 2, 2023, 5:55 PM +04:00

--- hours/hours.c
+++ hours/hours.c
@@ -27,5 +27,5 @@ int main(void)
 // TODO: complete the calc_hours function
 float calc_hours(int hours[], int weeks, char output)
 {
-    
+    calc_hours = hours[week 0] + hours[week 1] + hours[week 2]
 }
