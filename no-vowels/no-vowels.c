eb1c34be0bc99d4e8c6e5d3654732b07e16b5efc
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-08-02 21:43:41 +0000

    Thu, Aug 3, 2023, 1:43 AM +04:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -10,11 +10,11 @@ int main(int argc, string argv[])
 {
     if (argc == 2)
     {
-        printf("hello, %s\n", argv[1]);
+        printf(", %s\n", argv[1]);
     }
     else
     {
-        printf("hello, world\n");
+        retunr 1;
     }
 }
 }
