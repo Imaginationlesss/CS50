4921951af5b1ae8f30ced0bbed7c34b49f46971e
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-08-02 21:49:09 +0000

    Thu, Aug 3, 2023, 1:49 AM +04:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -6,12 +6,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
+string replace(string input);
+
 int main(int argc, string argv[])
 {
     if (argc != 2)
     {
-        printf("Wrong info/n");
+        printf("Wrong argument/n");
         retunr 1;
+    }
+
+    string replace(string input){
+        
     }
 }
 }
