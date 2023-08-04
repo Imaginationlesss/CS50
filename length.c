aecfadddb89332230baab2e157f43ddbbc64a9d4
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-08-04 23:58:36 +0000

    Sat, Aug 5, 2023, 3:58 AM +04:00

--- length.c
+++ length.c
@@ -1,16 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctyope.h>
 
 int main (void)
 {
     string name =get_string("What's your name? ");
 
-    int n = 0;
-
-    while (name[n] != '\0')
-    {
-        n++;
-    }
+    int n = strlen(name);
 
     printf("%i\n", n);
 
