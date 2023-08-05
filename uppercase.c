3c76cf0f5c1d9ccfadb5d568a388e6b64963e507
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-08-05 00:03:23 +0000

    Sat, Aug 5, 2023, 4:03 AM +04:00

---
+++ uppercase.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctyope.h>
+
+int main(void)
+{
+    string s = get_string("Before ");
+
+    printf("After: ");
+
+    for (int i = 0; i < strlen(s); i++);
+    {
+        if (s[i] >= 'a' && s[i] <= 'z')
+        {
+            
+        }
+    }
+}
