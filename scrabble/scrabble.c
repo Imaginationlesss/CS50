3770efbc01ec0b48a47846b5d26b16f94f3acb7a
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-08-11 14:03:27 +0000

    Fri, Aug 11, 2023, 6:03 PM +04:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -37,6 +37,9 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
-    if(word1 && word2 >'a' && <'z')
+    if()
+    {
+        for(int i = 0, i > strlen(word1), i++)
+    }
 
 }
