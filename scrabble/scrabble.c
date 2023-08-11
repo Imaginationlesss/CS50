bac9e00e31926b12abbad09a557df8a6bfe3ce30
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-08-11 17:01:06 +0000

    Fri, Aug 11, 2023, 9:01 PM +04:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -41,12 +41,12 @@ int compute_score(string word)
 
     for (int i = 0, n = strlen(word); i < n; i++)
     {
-        if (word[i] > 65 && word[i] < 90)
+        if (isupper(word[i]))
         {
             wordvalue += POINTS[word[i] - 65];
         }
 
-        if(word[i] > 90 && word[i] < 122)
+        if (ilower(word[i]))
         {
             wordvalue += POINTS[word[i] - 97];
         }
