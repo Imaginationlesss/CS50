f2625e53f288ddae22160767843cabdebbad8c97
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-08-11 17:02:27 +0000

    Fri, Aug 11, 2023, 9:02 PM +04:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,11 +19,11 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
-    if (score1 < score2)
+    if (score1 > score2)
     {
         printf("Player 1 Wins! ");
     }
-    else
+    if else (score1 < score2)
     {
         printf("Player 2 Wins!");
     }
