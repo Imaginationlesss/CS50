747d6788aaed8186ea28277835feea27a67702cf
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-08-21 17:19:05 +0000

    Mon, Aug 21, 2023, 9:19 PM +04:00

--- readability/readability.c
+++ readability/readability.c
@@ -12,57 +12,72 @@ int main(void)
 {
     // Getting input text
     string text = get_string("Text: ");
-    printf("Text: %s\n", text);
-    count_letters(text);
-    count_words(text);
-    count_sentences(text);
-    calculationl();
-}
+    printf("Text: %s\n", text);\
+
+    int count_letters(text);
+    int count_words(text);
+    int count_sentences(text);
+    int calculationl();
+
+    if(X < 1)
+    {
+        printf("Before Grade\n");
+    }
+    else if(X >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade: %i\n", X);
+    }
+    }
 
 
     // Calculating number of letters in text
     int count_letters(string text){
-    int letters = 0;
-    for(int i = 0, n = strlen(text); i < n; i++)
-    {
-        if(((text[i]) >= 'a' && (text[i]) <= 'z') || ((text[i]) >= 'A' && (text[i]) <= 'Z'))
-        letters++;
+        int letters = 0;
+            for(int i = 0, n = strlen(text); i < n; i++)
+            {
+                if(((text[i]) >= 'a' && (text[i]) <= 'z') || ((text[i]) >= 'A' && (text[i]) <= 'Z'))
+                letters++;
 
-    }
-    return letters;
+            }
+        return letters;
     }
 
     //Calculating number of words in text
     int count_words(string text){
         int words = 1;
-        for(int i = 0, n = strlen(text); i < n; i++)
-        {
-            if((text[i]) == ' ')
-            words++;
-        }
+            for(int i = 0, n = strlen(text); i < n; i++)
+            {
+                if((text[i]) == ' ')
+                words++;
+            }
         return words;
     }
 
     //Calculating number of sentences in text
     int count_sentences(string text){
-    int sentences = 0;
-        for(int i = 0, n = strlen(text); i < n; i++)
-        {
-            if(text[i] == '.' || text[i] == '?' || text[i] == '!')
-            sentences++;
+        int sentences = 0;
+            for(int i = 0, n = strlen(text); i < n; i++)
+            {
+                if(text[i] == '.' || text[i] == '?' || text[i] == '!')
+                sentences++;
 
-        }
-        return senteces;
+            }
+            return senteces;
     }
 
     //Using Coleman-Liau formula to calculate the grade of the text
-    int calculation(int){
-    float L = letters/words*100;
-    float S = sentences/words*100;
-    float index = 0.0588 * L - 0.296 * S - 15.8;
+    int calculation(int)
+    {
+        float L = letters/words*100;
+        float S = sentences/words*100;
+        float index = 0.0588 * L - 0.296 * S - 15.8;
 
-    int X = round(index);
+        int X = round(index);
 
-    printf("Grade: %i\n", X);
-}
+        return X;
+    }
 
