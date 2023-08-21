1e11b2c7e6d80697d94fa563d02d0c06e08dac9a
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-08-21 17:00:46 +0000

    Mon, Aug 21, 2023, 9:00 PM +04:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,18 +8,20 @@
 
 
 
-int main(int argc, string argv[])
+int main(void)
 {
     // Getting input text
     string text = get_string("Text: ");
     printf("Text: %s\n", text);
-    count_letters;
-    count_words;
-    count_sentences;
-    calculationl
+    count_letters();
+    count_words();
+    count_sentences();
+    calculationl();
+}
+
 
     // Calculating number of letters in text
-    int count_letters(string){
+    int count_letters(string text){
     int letters = 0;
     for(int i = 0, n = strlen(text); i < n; i++)
     {
@@ -27,27 +29,33 @@ int main(int argc, string argv[])
         letters++;
 
     }
+    return letters;
     }
 
     //Calculating number of words in text
-    int words = 1;
-    for(int i = 0, n = strlen(text); i < n; i++)
-    {
-        if((text[i]) == ' ')
-        words++;
-
+    int count_words(string text){
+        int words = 1;
+        for(int i = 0, n = strlen(text); i < n; i++)
+        {
+            if((text[i]) == ' ')
+            words++;
+        }
+        return words;
     }
 
     //Calculating number of sentences in text
+    int count_letters(string text){
     int sentences = 0;
-    for(int i = 0, n = strlen(text); i < n; i++)
-    {
-        if(text[i] == '.' || text[i] == '?' || text[i] == '!')
-        sentences++;
+        for(int i = 0, n = strlen(text); i < n; i++)
+        {
+            if(text[i] == '.' || text[i] == '?' || text[i] == '!')
+            sentences++;
 
+        }
     }
 
     //Using Coleman-Liau formula to calculate the grade of the text
+    int calculation(int){
     float L = letters/words*100;
     float S = sentences/words*100;
     float index = 0.0588 * L - 0.296 * S - 15.8;
@@ -55,6 +63,5 @@ int main(int argc, string argv[])
     int X = round(index);
 
     printf("Grade: %i\n", X);
-
 }
 
