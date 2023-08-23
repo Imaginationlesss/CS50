cb289f11772eba9a97d12e6cad53a3631ed8dc86
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-08-23 13:03:27 +0000

    Wed, Aug 23, 2023, 5:03 PM +04:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,13 +1,13 @@
 #include <cs50.h>
 #include <ctype.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
     string key = argv[1];
 
-    //Check the key
+    // Check the key
     if (key == 0)
     {
         printf("Usage: ./substitution KEY\n");
@@ -18,15 +18,14 @@ int main(int argc, string argv[])
     {
         if (!isalpha(key[i]))
         {
-        printf ("Key must contain alphabetic characters\n");
-        return 1;
+            printf("Key must contain alphabetic characters\n");
+            return 1;
         }
     }
 
-
-    if (strlen(key) != 26 )
+    if (strlen(key) != 26)
     {
-        printf ("Key must contain 26 characters\n");
+        printf("Key must contain 26 characters\n");
         return 1;
     }
 
@@ -36,27 +35,27 @@ int main(int argc, string argv[])
         {
             if (tolower(key[i]) == tolower(key[j]) || toupper(key[i]) == toupper(key[j]))
             {
-            printf ("Key must not contain repeated characters\n");
-            return 1;
+                printf("Key must not contain repeated characters\n");
+                return 1;
             }
         }
     }
-    //Receive input
-    string plaintext = get_string ("plaintext: ");
+    // Receive input
+    string plaintext = get_string("plaintext: ");
 
     string cipertext = 0;
 
-    //Transform into cipertext
+    // Transform into cipertext
 
     for (int i = 0, n = strlen(plaintext); i < n; i++)
     {
         int difference = 1;
-        if(isupper(plaintext[i]))
+        if (isupper(plaintext[i]))
         {
             difference += plaintext[i] - 65;
             cipertext += key[difference];
         }
-        else if(islower(plaintext[i]))
+        else if (islower(plaintext[i]))
         {
             difference += plaintext[i] - 90;
             cipertext += key[difference];
@@ -66,12 +65,10 @@ int main(int argc, string argv[])
     printf("cipertext: %s\n", cipertext);
     return 0;
 
-    //VCHPRZGJNTLSKFBDQWAXEUYMOI
-    //ABCDEFGHIJKLMNOPQRSTUVWXYZ
+    // VCHPRZGJNTLSKFBDQWAXEUYMOI
+    // ABCDEFGHIJKLMNOPQRSTUVWXYZ
 
-
-
-    //for each character in plaintext
-    //find corresponding character in key
-    //add corresponding character to ciphertext
+    // for each character in plaintext
+    // find corresponding character in key
+    // add corresponding character to ciphertext
 }
