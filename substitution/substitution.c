c7e4b0c69497a2ba94e23f811e741fbf710ee095
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-08-23 11:11:43 +0000

    Wed, Aug 23, 2023, 3:11 PM +04:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -8,32 +8,38 @@ int main(int argc, string argv[])
     string key = argv[1];
 
     //Check the key
-    if(key == 0)
+    if (key == 0)
     {
         printf("Usage: ./substitution KEY\n");
         return 1;
     }
 
-
-    for(int i = 0, n = strlen(key); i < n; i++)
+    for (int i = 0, n = strlen(key); i < n; i++)
     {
-        if(!isalpha(key[i]))
+        if (!isalpha(key[i]))
         {
-
-        printf("Key must contain alphabetic characters\n");
-
+        printf ("Key must contain alphabetic characters\n");
         return 1;
         }
     }
 
 
-    if(strlen(key) != 26 )
+    if (strlen(key) != 26 )
     {
-        printf("Key must contain 26 characters\n");
+        printf ("Key must contain 26 characters\n");
         return 1;
     }
+
+    
     //Receive input
-    string plaintext = get_string("plaintext: ");
+    string plaintext = get_string ("plaintext: ");
 
     //Check the input
+    for (int i = 0, n = strlen(plaintext); i < n; i++)
+    {
+        if (!isalpha(plaintext[i]))
+        {
+        printf ("Text must contain alphabetic characters\n");
+        return 1;
+        }
 }
