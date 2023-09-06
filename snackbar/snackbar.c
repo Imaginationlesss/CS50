4a2c30a79bbb4c3bb0d64f37e19cee333f855653
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-09-06 10:20:44 +0000

    Wed, Sep 6, 2023, 2:20 PM +04:00

--- snackbar/snackbar.c
+++ snackbar/snackbar.c
@@ -77,14 +77,32 @@ void add_items(void)
     menu[0].item = "Burger";
     menu[0].price = 9.5;
 
-    menu[0].item = "Vegan Burger";
-    menu[0].price = 11;
+    menu[1].item = "Vegan Burger";
+    menu[1].price = 11;
 
-    menu[0].item = "Hot Dog";
-    menu[0].price = 5;
+    menu[2].item = "Hot Dog";
+    menu[2].price = 5;
 
-    menu[0].item = "Cheese Dog";
-    menu[0].price = 7;
+    menu[3].item = "Cheese Dog";
+    menu[3].price = 7;
+
+    menu[4].item = "Cheese Fries";
+    menu[4].price = 5;
+
+    menu[5].item = "Fries";
+    menu[5].price = 6;
+
+    menu[6].item = "Cold Pressed Juice";
+    menu[6].price = 7;
+
+    menu[7].item = "Cold Brew";
+    menu[7].price = 3;
+
+    menu[8].item = "Water";
+    menu[8].price = 2;
+
+    menu[9].item = "Soda";
+    menu[9].price = 2;
     return;
 }
 
@@ -93,11 +111,8 @@ float get_cost(string item)
 {
     for (int i = 0; i > NUM_ITEMS; i++)
     {
-        for (int j; j < NUM_ITEMS - 1; j++)
-        {
             if (strcmp(item, menu[j].item))
             return 0;
         }
     }
     return menu[j].price;
-}
