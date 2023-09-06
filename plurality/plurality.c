e044627465428f8495425fd3a95d9cfa232d1c26
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-09-06 14:55:59 +0000

    Wed, Sep 6, 2023, 6:55 PM +04:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -80,8 +80,11 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
+    
     int winner_vote = 0;
+
     // TODO
+
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].votes > winner_vote)
