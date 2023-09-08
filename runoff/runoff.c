0bd38345f190cbf499fa31bcc109515ed5bc6e54
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-09-08 23:21:52 +0000

    Sat, Sep 9, 2023, 3:21 AM +04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,8 +130,9 @@ bool vote(int voter, int rank, string name)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp (name, candidate name))
+        if (strcmp (name, candidates[i].name) == 0)
         {
+            candidates[i].votes++
             
         }
     }
