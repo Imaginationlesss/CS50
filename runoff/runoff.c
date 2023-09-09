0eb1f865ec562c3b79d42561e8564539296885d1
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-09-09 00:03:28 +0000

    Sat, Sep 9, 2023, 4:03 AM +04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -148,7 +148,7 @@ void tabulate(void)
     // TODO
     for (int i = 0; i < voter_count; i++)
     {
-        for (int j = 0; j < voter_count; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
             if (eliminated == true)
             {
