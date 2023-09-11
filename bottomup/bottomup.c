1152973046d44b5de60b19ed33acd09e28f40ae8
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-09-11 08:52:44 +0000

    Mon, Sep 11, 2023, 12:52 PM +04:00

--- bottomup/bottomup.c
+++ bottomup/bottomup.c
@@ -53,6 +53,10 @@ int main(int argc, char *argv[])
         return 4;
     }
 
+
+    //Todo
+    bi.biHeight = bi.biHeight * -1;
+
     // Write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
@@ -61,7 +65,6 @@ int main(int argc, char *argv[])
 
     // Determine padding for scanlines
     int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
-
 
     // Iterate over infile's scanlines
     for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
