35f86b345164474809734e25843e858ffbd90e61
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-09-11 08:55:36 +0000

    Mon, Sep 11, 2023, 12:55 PM +04:00

--- bottomup/bottomup.c
+++ bottomup/bottomup.c
@@ -44,8 +44,7 @@ int main(int argc, char *argv[])
     fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
 
     // Ensure infile is (likely) a 24-bit uncompressed BMP 4.0
-    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
-        bi.biBitCount != 24 || bi.biCompression != 0)
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || bi.biBitCount != 24 || bi.biCompression != 0)
     {
         fclose(outptr);
         fclose(inptr);
@@ -53,8 +52,7 @@ int main(int argc, char *argv[])
         return 4;
     }
 
-
-    //Todo
+    // Todo
     bi.biHeight = bi.biHeight * -1;
 
     // Write outfile's BITMAPFILEHEADER
