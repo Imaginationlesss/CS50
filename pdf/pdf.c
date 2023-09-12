0766051c0057744c6cd9b6fb1b54edf3d166d4d8
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-09-12 04:45:10 +0000

    Tue, Sep 12, 2023, 8:45 AM +04:00

--- pdf/pdf.c
+++ pdf/pdf.c
@@ -6,18 +6,25 @@ int main(int argc, string argv[])
 {
     if ( argc != 2)
     {
-        printf("Improper usage.")
+        printf("Improper usage.");
         return 1;
     }
 
     FILE *input = fopen (argv[1], "r");
 
-    if (file == NULL)
+    if (input == NULL)
     {
         printf("No such file found.\n");
         return 1;
     }
 
-    uint8_t =  
+    uint8_t buffer[4];
+
+    fread(buffer, 1, 4, input);
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%i\n", buffer[i]);
+    }
 
 }
