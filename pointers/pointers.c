1ee9da8463bfa54e7669dadf7adeb0033de5e171
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-09-12 03:44:50 +0000

    Tue, Sep 12, 2023, 7:44 AM +04:00

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -9,10 +9,10 @@ int main(void)
 
     *c = 14; &a = 14
     c = &b;   c = 50
-    *c = 25;   
+    *c = 25;
 
     // Print results
-    printf("a has the value %i, located at %p\n", a, &a);
+    printf("a has the value %i, located at %p\n", a, &a);  20  28
     printf("b has the value %i, located at %p\n", b, &b);
     printf("c has the value %p, located at %p\n", c, &c);
 }
