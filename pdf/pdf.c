75605b65068fa310cb0fbc3c95c11ff59ed8929e
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-09-12 04:15:04 +0000

    Tue, Sep 12, 2023, 8:15 AM +04:00

--- pdf/pdf.c
+++ pdf/pdf.c
@@ -4,16 +4,5 @@
 
 int main(int argc, string argv[])
 {
-    FILE *input = fopen(argv[1], "r");
-
-    uint8_t buffer[4];
-
-    fread(buffer, 1, 4, input);
-
-    for (int i = 0; i < 4; i++)
-    {
-        printf("%i\n", buffer[i]);
-    }
-
-    fclose(input);
+    
 }
