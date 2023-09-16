ff91a6a83755a8a152500e39dddf5e0367dce3a4
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-09-16 13:09:07 +0000

    Sat, Sep 16, 2023, 5:09 PM +04:00

--- recover/recover.c
+++ recover/recover.c
@@ -28,7 +28,8 @@ int main(int argc, char *argv[])
         if (buffer[0] = 0xff && buffer[1] = 0xd8 && buffer[2] = 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             sprintf(outp, "%03i.jpg", image_counter);
-            fwrite()
+            output_file = fopen(outp, "w")
+            image_counter++;
         }
 
 
