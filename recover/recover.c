3a0f1f23a7fbe3abb87a4f795917625725495a18
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-09-16 15:54:38 +0000

    Sat, Sep 16, 2023, 7:54 PM +04:00

--- recover/recover.c
+++ recover/recover.c
@@ -40,6 +40,7 @@ int main(int argc, char *argv[])
             else
             {
                 found_jpg = true;
+                fclose(output_file);
             }
             char output[8];
             sprintf(output, "%03i.jpg", image_counter);
