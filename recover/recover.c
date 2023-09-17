5e3696bb98aad1dcda5fef061670498ef0abef47
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-09-17 13:50:31 +0000

    Sun, Sep 17, 2023, 5:50 PM +04:00

--- recover/recover.c
+++ recover/recover.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 
     // Read the file of 512bytes and store into buffer until the end of the card
     FILE *output_file = NULL;
-    
+
     while (fread(buffer, sizeof(BYTE), BLOCK_SIZE, input_file) == 1)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
@@ -44,11 +44,11 @@ int main(int argc, char *argv[])
                 found_jpg = true;
             }
             sprintf(output, "%03i.jpg", image_counter);
-            output_file = fopen(output, "a");
+            output_file = fopen(output, "w");
             if (output_file == NULL)
             {
+                printf("Could not create the file%s\n", output)
                 fclose(input_file);
-                printf("Could not create the file");
                 return 2;
             }
             image_counter++;
@@ -59,10 +59,13 @@ int main(int argc, char *argv[])
             fwrite(buffer, sizeof(BYTE), BLOCK_SIZE, output_file);
         }
     }
+
+    fclose(input_file);
+
     if (found_jpg)
     {
-    fclose(output_file);
+        fclose(output_file);
     }
-    fclose(input_file);
+    
     return 0;
 }
