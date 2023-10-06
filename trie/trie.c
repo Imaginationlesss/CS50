eaf3f4799bc5d3102e746bc0fddb8c876dca1d2c
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-10-06 13:22:03 +0000

    Fri, Oct 6, 2023, 5:22 PM +04:00

--- trie/trie.c
+++ trie/trie.c
@@ -110,7 +110,12 @@ int main(int argc, char *argv[])
 // TODO: Complete the check function, return true if found, false if not found
 bool check(char* word)
 {
-    
+    node *cursor = root;
+
+    for (int i = 0, n = strlen(word), i < n; i++)
+    {
+        
+    }
     return false;
 }
 
