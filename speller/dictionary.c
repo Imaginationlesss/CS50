520aed07f8319484fd88dd824773148af37d3e80
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-10-18 03:14:17 +0000

    Wed, Oct 18, 2023, 7:14 AM +04:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -106,10 +106,10 @@ bool unload(void)
     // TODO
     for (int i = 0; i < N; i++)
     {
-
     node *cursor = table[i];
     node *tmp = cursor;
     cursor = cursor->next;
-    free(cursor)
+    free(cursor);
+    free(tmp);
     return false;
 }
