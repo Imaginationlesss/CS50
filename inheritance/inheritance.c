fa23809892cdf8866514a6946059e57a7a7cdb0d
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-10-21 02:34:21 +0000

    Sat, Oct 21, 2023, 6:34 AM +04:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -10,8 +10,7 @@ typedef struct person
 {
     struct person *parents[2];
     char alleles[2];
-}
-person;
+} person;
 
 const int GENERATIONS = 3;
 const int INDENT_LENGTH = 4;
@@ -57,8 +56,8 @@ person *create_family(int generations)
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
 
-        p->alleles[0] = parent0->allels[rand() % 2];
-        p->alleles[1] = parent1->allels[rand() % 2];
+        p->alleles[0] = parent0->alleles[rand() % 2];
+        p->alleles[1] = parent1->alleles[rand() % 2];
     }
 
     // If there are no generations left to create
@@ -71,8 +70,8 @@ person *create_family(int generations)
 
         // TODO: Randomly assign alleles
 
-        p->alleles[0] = random_allele;
-        p->alleles[1] = random_allele;
+        p->alleles[0] = random_allele();
+        p->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
@@ -83,13 +82,19 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-    
-    
+
+    if (p == NULL)
+    {
+        return;
+    }
 
     // TODO: Free parents recursively
 
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
     // TODO: Free child
 
+    free(p);
 }
 
 // Print each family member and their alleles.
