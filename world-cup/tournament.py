e8111b02a2187f10de451152d750a70bf71389d1
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-10-26 13:41:16 +0000

    Thu, Oct 26, 2023, 1:41 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -57,6 +57,9 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
+    while len(teams) > 1:
+        simulate_round(teams)
+    return teams[0]['team']
 
 
 
