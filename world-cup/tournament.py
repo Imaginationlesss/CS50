d5ce17dff60c335b1f34666699ed27fce120e4f9
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-10-26 13:22:39 +0000

    Thu, Oct 26, 2023, 1:22 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -18,10 +18,13 @@ def main():
     # TODO: Read teams into memory from file
     with open("2018m.csv") as FifaMen:
         ReadFifaMen = csv.DictReader(FifaMen)
+        for team in ReadFifaMen:
+            teams.append(team)
 
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
+    
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
