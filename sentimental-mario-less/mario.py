b2b6b649db7119d139828c79904f12175230d96a
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-10-29 11:59:07 +0000

    Sun, Oct 29, 2023, 11:59 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -5,14 +5,15 @@ while True:
     height = input("Height: ")
     if len(height) == 0:
         print("Please write correct height between 1 and 8!")
-    try:
-        height = int(height)
-        if height >= 1 and height <= 8:
-            break
-        else:
+    else:
+        try:
+            height = int(height)
+            if height >= 1 and height <= 8:
+                break
+            else:
+                print("Please write correct height between 1 and 8!")
+        except ValueError:
             print("Please write correct height between 1 and 8!")
-    except ValueError:
-        print("Please write correct height between 1 and 8!")
 
 
 for i in range(height):
