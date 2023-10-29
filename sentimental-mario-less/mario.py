7efeb20a94b337dffa92de972dd2bebb0b4a36e2
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-10-29 10:47:09 +0000

    Sun, Oct 29, 2023, 10:47 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -4,8 +4,7 @@
 while True:
     height = int(input("Height: "))
     if(height < 1 or height > 8):
-        break
-    return height
+        height = int(input("Height: "))
 
 
 for i in range(height):
