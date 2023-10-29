75b567786046b7efd02def4c02b9ed284bee0fa3
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-10-29 11:10:11 +0000

    Sun, Oct 29, 2023, 11:10 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -2,18 +2,14 @@
 
 
 height = int(input("Height: "))
-while(height < 1 or height > 8):
+while height < 1 or height > 8:
     height = int(input("Height: "))
 
 
 for i in range(height):
     for j in range(height):
-        if (i + j < height - 1):
-            print(" ", end = "")
+        if i + j < height - 1:
+            print(" ", end="")
         else:
-            print("#", end = "")
+            print("#", end="")
     print()
-
-
-
-
