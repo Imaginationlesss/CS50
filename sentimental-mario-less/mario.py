eda90a939efb3d2153f454cbb5a8651c3b2e357b
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-10-29 11:47:54 +0000

    Sun, Oct 29, 2023, 11:47 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,16 +1,16 @@
 # TODO
 
-import sys
+height = 0
 
-if len(sys.argv) != 2:
-    print("Invalid Usage")
-    sys.exit(1)
 
 while True:
-    height = int(sys.argv[1])
-    if height >= 1 and height <= 8:
-        break
-    else:
+    height = int(input("Height: "))
+    try:
+        if height >= 1 and height <= 8:
+            break
+        else:
+            print("Please write correct height between 1 and 8!")
+    except ValueError:
         print("Please write correct height between 1 and 8!")
 
 
