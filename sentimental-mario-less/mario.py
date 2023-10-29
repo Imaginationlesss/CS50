97a394074e7c120c11b60c2ab25398bc54edaf2e
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-10-29 10:58:18 +0000

    Sun, Oct 29, 2023, 10:58 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -9,7 +9,7 @@ while(height < 1 or height > 8):
 for i in range(height):
     for j in range(height):
         if (i + j < height - 1):
-            print(" ", end="")
+            print(" ")
         else:
             print("#")
 
