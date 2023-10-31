a40e118a9f2a545abbbb6d65ea1662c25a94b6d7
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-10-31 13:18:26 +0000

    Tue, Oct 31, 2023, 1:18 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,4 +3,10 @@
 from cs50 import get_int
 
 while true:
-    credit_card = get_int("Enter credit card number:")
+    credit_card_number = get_int("Enter credit card number:")
+
+    if not card_number:
+        break
+
+    if valid_card(credit_card_number):
+        card_type = 
