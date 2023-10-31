839e9eca910f328e313fba46783b914998f20b85
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-10-31 14:19:30 +0000

    Tue, Oct 31, 2023, 2:19 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,19 +3,26 @@
 from cs50 import get_string
 
 
-def get_credit_card_number():
-    redit_card_number = credit_card_number.replace(" ", "")
-
-    if not credit_card_number.isdigit():
-        return "Invalid! Please type in only digits!"
-    
-
-
-def get_card_type(credit_card_number):
+def get_credit_card_number(credit_card_number):
     credit_card_number = credit_card_number.replace(" ", "")
 
     if not credit_card_number.isdigit():
         return "Invalid! Please type in only digits!"
+
+
+
+def get_card_type():
+    while True:
+        credit_card_number = get_string("Enter credit card number:")
+
+        if not credit_card_number:
+            break
+
+        card_type = get_card_type(credit_card_number)
+        if card_type != "Invalid":
+            print(f"Card Type: {card_type}")
+        else:
+            print("Invalid credit card number")
 
     if len(credit_card_number) == 15 and credit_card_number[0:2] in ["34", "37"]:
         return "AMEX"
