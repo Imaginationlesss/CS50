04919f04a6cd936c6c307a96290f269a6ff66687
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-10-31 14:23:06 +0000

    Tue, Oct 31, 2023, 2:23 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,14 +3,14 @@
 from cs50 import get_string
 
 
-def get_credit_card_number(credit_card_number):
+def is_valid_credit_card(credit_card_number):
     credit_card_number = credit_card_number.replace(" ", "")
 
     if not credit_card_number.isdigit():
         return "Invalid! Please type in only digits!"
 
 
-def get_card_type():
+def get_credit_card_number():
     while True:
         credit_card_number = get_string("Enter credit card number:")
 
@@ -19,14 +19,17 @@ def get_card_type():
 
         card_type = get_card_type(credit_card_number)
         if card_type != "Invalid":
-            
 
-    if len(credit_card_number) == 15 and credit_card_number[0:2] in ["34", "37"]:
-        return "AMEX"
-    if len(credit_card_number) == 16:
-        if credit_card_number[0] == "4":
-            return "VISA"
-        if "51" <= credit_card_number[0:2] <= "55":
-            return "MASTERCARD"
+
+        if len(credit_card_number) == 15 and credit_card_number[0:2] in ["34", "37"]:
+            return "AMEX"
+        elif len(credit_card_number) == 16:
+            if credit_card_number[0] == "4":
+                return "VISA"
+            elif "51" <= credit_card_number[0:2] <= "55":
+                return "MASTERCARD"
+            print(f"Card Type: {card_type}")
+        else:
+            print("Invalid credit card number")
 
     return "INVALID"
