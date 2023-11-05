f949193eb2987b3da53127f817e7106b993266ce
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-05 16:15:57 +0000

    Sun, Nov 5, 2023, 4:15 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,3 +5,7 @@ from cs50 import get_string
 def main():
     text = get_string("Text: ")
 
+letters = count_letters(text)
+words = count_words(text)
+sentences = count_sentences(text)
+index = calculation(letters, words, sentences)
