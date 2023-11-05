1128474ccf100eff1234b5a82efae4fec4c7a9aa
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-05 16:21:58 +0000

    Sun, Nov 5, 2023, 4:21 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,7 +5,21 @@ from cs50 import get_string
 def main():
     text = get_string("Text: ")
 
-letters = count_letters(text)
-words = count_words(text)
-sentences = count_sentences(text)
-index = calculation(letters, words, sentences)
+    letters = count_letters(text)
+    words = count_words(text)
+    sentences = count_sentences(text)
+    index = calculation(letters, words, sentences)
+
+    if index < 1:
+        print("Before Grade 1")
+    elif index >= 16:
+        print("Grade 16+")
+    else:
+        print(f"Grade {index}")
+
+
+def count_letters(text):
+    
+
+
+
