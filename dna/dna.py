b45eaf6fb0a213f6982c7d03ae8c6dc1bfcedc19
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-06 22:49:35 +0000

    Mon, Nov 6, 2023, 10:49 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,7 +5,7 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if  len(argv) != 3:
+    if  len(sys.argv) != 3:
         print("Incorect number of command-line arguments")
 
     # TODO: Read database file into a variable
