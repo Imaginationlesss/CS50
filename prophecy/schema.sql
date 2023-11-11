f05893b0710469b6b70cd4bb82e499507d79dd36
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-11 23:27:41 +0000

    Sat, Nov 11, 2023, 11:27 PM +00:00

--- prophecy/schema.sql
+++ prophecy/schema.sql
@@ -1,6 +1,11 @@
 CREATE TABLE students (
     id INTEGER NOT NULL,
     student TEXT NOT NULL,
-    
+    PRIMARY KEY(id)
+);
 
-)
+CREATE TABLE houses (
+    id INTEGER NOT NULL
+    house TEXT NOT NULL,
+    PRIMARY KEY(id)
+);
