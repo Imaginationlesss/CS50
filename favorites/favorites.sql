31abdb88e291e63b21c8beb81306ade30eb2d613
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-11 22:21:00 +0000

    Sat, Nov 11, 2023, 10:21 PM +00:00

--- favorites/favorites.sql
+++ favorites/favorites.sql
@@ -1,1 +1,23 @@
-UPDATE shows SET tittle 
+UPDATE shows SET title = "Adventure Time" WHERE title LIKE "Adventure time";
+UPDATE shows SET title = "Arrow" WHERE title LIKE "Arrow";
+UPDATE shows SET title = "Avatar: The Last Airbender" WHERE title LIKE "Avatar: The Last Airbender";
+UPDATE shows SET title = "Brooklyn Nine-Nine" WHERE title LIKE "Brooklyn Nine-Nine";
+UPDATE shows SET title = "Community" WHERE title LIKE "Community";
+UPDATE shows SET title = "Family Guy" WHERE title LIKE "Family Guy";
+UPDATE shows SET title = "Friends" WHERE title LIKE "Friends";
+UPDATE shows SET title = "Game Of Thrones" WHERE title LIKE "Game Of Thrones";
+UPDATE shows SET title = "Gilmore Girls" WHERE title LIKE "Gilmore Girls";
+UPDATE shows SET title = "Grey's Anatomy" WHERE title LIKE "Grey's Anatomy";
+UPDATE shows SET title = "How I Meet Your Mother" WHERE title LIKE "How I Meet Your Mother";
+UPDATE shows SET title = "It's Always Sunny In Philadelphia" WHERE title LIKE "It's Always Sunny In Philadelphia";
+UPDATE shows SET title = "Parks and Recreation" WHERE title LIKE "Parks and Recration";
+UPDATE shows SET title = "Sherlock" WHERE title LIKE "Sherlock";
+UPDATE shows SET title = "Squid Game" WHERE title LIKE "Squid Game";
+UPDATE shows SET title = "The Bachelorette" WHERE title LIKE "The Bachelorette";
+UPDATE shows SET title = "The Crown" WHERE title LIKE "The Crown";
+UPDATE shows SET title = "The Office" WHERE title LIKE "The Office";
+UPDATE shows SET title = "The Queen's Gambit" WHERE title LIKE "The Queen's Gambit";
+UPDATE shows SET title = "The Untamed" WHERE title LIKE "The Untamed";
+
+SELECT * FROM shows;
+
