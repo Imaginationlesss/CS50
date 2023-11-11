e6bb2397ca4c0afcb3c1157df69d281a81ac8bcb
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-11 23:46:10 +0000

    Sat, Nov 11, 2023, 11:46 PM +00:00

--- prophecy/hogwarts.py
+++ prophecy/hogwarts.py
@@ -1,7 +1,7 @@
 import csv
 
-with open ('students.csv', "r") as csvfile:
+with open("students.csv", "r") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(f"reader" {reader})
+        print(row)
 
