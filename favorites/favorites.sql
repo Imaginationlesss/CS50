8fc5c71735cfd54e832656f54f0981a137d547a7
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-11 22:40:54 +0000

    Sat, Nov 11, 2023, 10:40 PM +00:00

--- favorites/favorites.sql
+++ favorites/favorites.sql
@@ -23,7 +23,7 @@ UPDATE shows SET title = "Brooklyn Nine-Nine" WHERE title LIKE "B99";
 UPDATE shows SET title = "Brooklyn Nine-Nine" WHERE title LIKE "Brooklyn%";
 UPDATE shows SET title = "Game Of Thrones" WHERE title LIKE "GoT";
 UPDATE shows SET title = "Grey's Anatomy" WHERE title LIKE "Grey's%";
-UPDATE shows SET title = "It's Always Sunny In Philadelphia" WHERE title LIKE "It's Always%"
+UPDATE shows SET title = "It's Always Sunny In Philadelphia" WHERE title LIKE "%Always Sunny%";
 UPDATE shows SET title = "Parks and Recreation" WHERE title LIKE "Parks and%";
 UPDATE shows SET title = "The Office" WHERE title LIKE "%Office";
 
