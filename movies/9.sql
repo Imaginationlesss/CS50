f05f15aca42c24cf68127db824301dbb16d76b0d
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-14 23:35:16 +0000

    Tue, Nov 14, 2023, 11:35 PM +00:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT name FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
-AND year = "2014"
+WHERE movies.year = "2014"
 ORDER BY people.birth ASC;
