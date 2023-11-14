ca42f93e37b1c79c9edf13e7b4703bbff434cc54
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-14 23:22:08 +0000

    Tue, Nov 14, 2023, 11:22 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,6 +1,10 @@
-SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON stars.movie_id = movies.id WHERE people.name = "Kevin Bacon" AND people.birth = "1958"
 
 SELECT DISTINCT name FROM people
 JOIN stars ON stars.person_id = people.id
 JOIN movies ON stars.movie_id = movies.id
-WHERE movies.id IN (SELECT movies.id FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE people.name IN ("Kevin Bacon") AND people.birth = "1958") AND people.name != "Kevin Bacon" ORDER BY name;
+WHERE movies.id IN
+ (SELECT movies.id FROM movies
+ JOIN stars ON movies.id = stars.movie_id
+ JOIN people ON stars.person_id = people.id
+ WHERE people.name IN ("Kevin Bacon") AND people.birth = "1958")
+ AND people.name != "Kevin Bacon" ORDER BY name;
