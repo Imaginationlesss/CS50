1a5c966732c981473f21acbd5725612ed47144fb
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-14 22:57:39 +0000

    Tue, Nov 14, 2023, 10:57 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,3 @@
-SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON stars.movie_id = movies.id WHERE people
+SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON stars.movie_id = movies.id WHERE people.name = "Kevin Bacon" AND people.birth = "1958"
+
+SELECT 
