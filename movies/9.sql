5e6b2cb4bdcfbd4cee1d5be29c62060c0e6f57f2
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-14 23:26:44 +0000

    Tue, Nov 14, 2023, 11:26 PM +00:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,1 +1,5 @@
-SELECT DISTINCT people.name FROM people, stars, movies WHERE people.id = stars.movie_id AND stars.movie_id = movies.id AND year = "2014" ORDER BY people.birth;
+SELECT DISTINCT people.name FROM people, stars, movies
+WHERE people.id = stars.movie_id
+AND stars.movie_id = movies.id
+AND year = "2014"
+ORDER BY people.birth;
