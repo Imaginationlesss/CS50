75bffef6c91228840bb20d9b34f86248d0739ee1
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-14 23:10:54 +0000

    Tue, Nov 14, 2023, 11:10 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -3,4 +3,4 @@ SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON
 SELECT name FROM people
 JOIN stars ON stars.person_id = people.id
 JOIN movies ON stars.movie_id = movies.id
-WHERE movies.title = (SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE people.name IN ("Kevin Bacon") AND people.birth = "1958");
+WHERE movies.title IN (SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE people.name IN ("Kevin Bacon") AND people.birth = "1958");
