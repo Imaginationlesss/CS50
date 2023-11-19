05a967dae2dc8941e9add5ba96f9d306dda4a365
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-19 05:28:15 +0000

    Sun, Nov 19, 2023, 5:28 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,3 +2,6 @@
 
 -- Seeing description of crime scene on 28th of july 2021 on Humphrey Street
 SELECT description FROM crime_scene_reports WHERE day = 28 AND month = 7 AND street LIKE "Humphrey%";
+
+--Checking the transcripts from interviews where bakery is mentioned
+SELECT transcript FROM interviews WHERE day = 28 AND month = 7 AND year = 2021;
