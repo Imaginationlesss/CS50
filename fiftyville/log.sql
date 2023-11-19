65d28e56502f5792f4948773ef4f4fbce223ddd1
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-19 05:20:43 +0000

    Sun, Nov 19, 2023, 5:20 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Seeing description of crime scene on 28th of july 2021
-SELECT description FROM crime_scene_reports WHERE day = 28 AND month = 7;
+SELECT description FROM crime_scene_reports WHERE day = 28 AND month = 7 AND street = "Humphrey Street%";
