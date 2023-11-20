113888b721df7e39ca2e43327c7fb3e4d9dabe10
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-20 07:35:10 +0000

    Mon, Nov 20, 2023, 7:35 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -49,7 +49,29 @@ FROM people WHERE name IN
    AND minute BETWEEN 15 AND 25);
 
 --Comparing the phone numbers of callers of specific information close to our suspect to the ones with the same name license plate numbers and name to distinguish the caller aka thief
-SELECT caller FROM phone_calls WHERE 
+SELECT caller FROM phone_calls
+WHERE caller IN
+ (SELECT phone_number
+FROM people WHERE name IN
+ (SELECT DISTINCT name FROM people
+ JOIN bakery_security_logs
+ ON people.license_plate = bakery_security_logs.license_plate
+ WHERE people.license_plate IN
+  (SELECT license_plate
+  FROM bakery_security_logs
+  WHERE day = 28
+  AND month = 7
+  AND year = 2021
+  AND hour = 10
+  AND minute BETWEEN 15 AND 25))
+  AND license_plate IN
+   (SELECT license_plate
+   FROM bakery_security_logs
+   WHERE day = 28
+   AND month = 7
+   AND year = 2021
+   AND hour = 10
+   AND minute BETWEEN 15 AND 25));
 
 
 
