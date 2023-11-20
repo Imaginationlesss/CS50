38c1d8932193be5be0be6eded7ead7ebe66b2d9b
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-20 09:23:58 +0000

    Mon, Nov 20, 2023, 9:23 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -240,4 +240,33 @@ WHERE bank_accounts.person_id IN
   AND transaction_type = "withdraw"
   AND day = 28
   AND month = 7))
-  
+  name IN
+  (SELECT name FROM people WHERE phone_number IN
+ (SELECT caller FROM phone_calls
+WHERE caller IN
+ (SELECT phone_number
+FROM people WHERE name IN
+ (SELECT name FROM people
+ JOIN bakery_security_logs
+ ON people.license_plate = bakery_security_logs.license_plate
+ WHERE people.license_plate IN
+  (SELECT license_plate
+  FROM bakery_security_logs
+  WHERE day = 28
+  AND month = 7
+  AND year = 2021
+  AND hour = 10
+  AND minute BETWEEN 15 AND 25))
+  AND license_plate IN
+   (SELECT license_plate
+   FROM bakery_security_logs
+   WHERE day = 28
+   AND month = 7
+   AND year = 2021
+   AND hour = 10
+   AND minute BETWEEN 15 AND 25))
+   AND day = 28
+   AND month = 7
+   AND year = 2021
+   AND duration < 60));
+
