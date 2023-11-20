48d5a266d827743d14e61965bb26c5f21be0cfa4
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-20 11:01:40 +0000

    Mon, Nov 20, 2023, 11:01 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -147,6 +147,7 @@ WHERE bank_accounts.person_id IN
                                 AND passport_number IN
                                     (SELECT id FROM flights
                                     WHERE origin_airport_id = 8
+                                    AND destio
                                     AND day = 29
                                     AND month = 7
                                     AND year = 2021
