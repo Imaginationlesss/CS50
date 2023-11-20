063c8feb925ede07250009fb984377402b2f8e11
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-20 10:18:27 +0000

    Mon, Nov 20, 2023, 10:18 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -69,6 +69,46 @@ WHERE phone_number IN
     AND hour = 10
     AND minute BETWEEN 15 AND 25));
 
+SELECT name FROM people
+JOIN bank_accounts ON bank_accounts.person_id = people.id
+WHERE bank_accounts.person_id IN
+ (SELECT person_id FROM bank_accounts
+ WHERE account_number IN
+  (SELECT account_number FROM atm_transactions
+  WHERE atm_location = "Leggett Street"
+  AND transaction_type = "withdraw"
+  AND day = 28
+  AND month = 7))
+  AND name IN
+  (SELECT name FROM people WHERE phone_number IN
+ (SELECT caller FROM phone_calls
+WHERE caller IN
+ (SELECT phone_number
+FROM people WHERE name IN
+ (SELECT name FROM people
+ JOIN bakery_security_logs
+ ON people.license_plate = bakery_security_logs.license_plate
+ WHERE people.license_plate IN
+  (SELECT license_plate
+  FROM bakery_security_logs
+  WHERE day = 28
+  AND month = 7
+  AND year = 2021
+  AND hour = 10
+  AND minute BETWEEN 15 AND 25))
+  AND license_plate IN
+   (SELECT license_plate
+   FROM bakery_security_logs
+   WHERE day = 28
+   AND month = 7
+   AND year = 2021
+   AND hour = 10
+   AND minute BETWEEN 15 AND 25))
+   AND day = 28
+   AND month = 7
+   AND year = 2021
+   AND duration < 60));
+   
 --Finding account number which thief used
 SELECT account_number FROM atm_transactions
 WHERE atm_location = "Leggett Street"
