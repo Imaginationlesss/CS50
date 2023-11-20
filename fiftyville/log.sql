f0aaf06577f708efd2068beead93041aee6c964d
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-20 08:23:32 +0000

    Mon, Nov 20, 2023, 8:23 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -179,8 +179,8 @@ SELECT seat from passengers WHERE flight_id IN
 
 
 SELECT name FROM people WHERE phone_number IN
- (SELECT DISTINCT caller FROM phone_calls
-WHERE receiver IN
+ (SELECT DISTINCT receiver FROM phone_calls
+WHERE caller IN
  (SELECT phone_number
 FROM people WHERE name IN
  (SELECT DISTINCT name FROM people
