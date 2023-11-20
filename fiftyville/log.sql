07abeef1495f23b45854c293056fd2e0e96a6311
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-11-20 10:15:09 +0000

    Mon, Nov 20, 2023, 10:15 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -69,59 +69,6 @@ WHERE phone_number IN
     AND hour = 10
     AND minute BETWEEN 15 AND 25));
 
-
---Getting id for Fiftyville airport
-SELECT id from airports WHERE city = "Fiftyville";
-
---Getting destination airport id
-SELECT id, destination_airport_id, hour, minute FROM flights WHERE origin_airport_id = 8 AND day = 29 AND month = 7 AND year = 2021 ORDER BY hour, minute ASC LIMIT 1;
-
---Getting Name of the City thief is going
-SELECT city FROM airports WHERE id = 4;
-
---Finding the thief on specific flight with passpoert number matching suspects
-SELECT passport_number FROM passengers
-WHERE flight_id IN
- (SELECT id FROM flights
- WHERE origin_airport_id = 8
- AND destination_airport_id = 4
- AND day = 29
- AND month = 7
- AND year = 2021
- AND hour = 8
- AND minute = 20)
- AND passport_number IN
-   (SELECT passport_number
-   FROM people WHERE name IN
-    (SELECT name FROM people WHERE phone_number IN
-     (SELECT DISTINCT caller FROM phone_calls
-     WHERE caller IN
-      (SELECT phone_number
-      FROM people WHERE name IN
-       (SELECT DISTINCT name FROM people
-       JOIN bakery_security_logs
-       ON people.license_plate = bakery_security_logs.license_plate
-       WHERE people.license_plate IN
-        (SELECT license_plate
-        FROM bakery_security_logs
-        WHERE day = 28
-        AND month = 7
-        AND year = 2021
-        AND hour = 10
-        AND minute BETWEEN 15 AND 25))
-        AND license_plate IN
-         (SELECT license_plate
-         FROM bakery_security_logs
-         WHERE day = 28
-         AND month = 7
-         AND year = 2021
-         AND hour = 10
-         AND minute BETWEEN 15 AND 25))
-         AND day = 28
-         AND month = 7
-         AND year = 2021
-         AND duration < 60)));
-
 --Finding account number which thief used
 SELECT account_number FROM atm_transactions
 WHERE atm_location = "Leggett Street"
