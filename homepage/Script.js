a4e8066db4feef145e1b96ee1857dd834c433155
Dimitri Lomsadze <119766737+Imaginationlesss@users.noreply.github.com>
2023-12-09 05:07:31 +0000

    Sat, Dec 9, 2023, 5:07 AM +00:00

--- homepage/Script.js
+++ homepage/Script.js
@@ -1,2 +1,20 @@
 
-var navbar = document.getElementById()
+var navbarhome = document.getElementById("Home");
+navbar.addEventListener("click", function() {
+    alert("Welcome, here you will read gereral information about me!")
+})
+
+var navbarHobby = document.getElementById("Hobbies");
+navbar.addEventListener("click", function() {
+    alert("Welcome, here you will read about my Hobbies!")
+})
+
+var navbarinterests = document.getElementById("Interests");
+navbar.addEventListener("click", function() {
+    alert("Welcome, here you will read about my Interests!")
+})
+
+var navbarmusic = document.getElementById("Music");
+navbar.addEventListener("click", function() {
+    alert("Welcome, here you will read about my favourtite Songs!")
+})
